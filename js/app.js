443a98ccc0d68253cd6526e337a9607b5033385e
TerenyaDanil <47912455+TerenyaDanil@users.noreply.github.com>
2021-08-12 19:14:40 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,121 +1,239 @@
+// Dynamic Adapt v.1
+// HTML data-da="where(uniq class name),position(digi),when(breakpoint)"
+// e.x. data-da="item,2,992"
+// Andrikanych Yevhen 2020
+// https://www.youtube.com/c/freelancerlifestyle
 
-if (window.innerWidth > 737) {
-	$(document).ready(function () {
-		$('#fullpage').fullpage({
-			//Navigation
+"use strict";
 
-			lockAnchors: false,
-			anchors: ['onePage', 'twoPage', 'threePage', 'fourPage', 'fivePage', 'sixPage'],
-			navigation: true,
-			navigationPosition: 'left',
-			afterLoad: function (anchorLink, index) {
-				if (index == onePage) {
-					$('.about__text, .about__video, .about__lines').addClass('active');
+(function () {
+	let originalPositions = [];
+	let daElements = document.querySelectorAll('[data-da]');
+	let daElementsArray = [];
+	let daMatchMedia = [];
+	//Заполняем массивы
+	if (daElements.length > 0) {
+		let number = 0;
+		for (let index = 0; index < daElements.length; index++) {
+			const daElement = daElements[index];
+			const daMove = daElement.getAttribute('data-da');
+			if (daMove != '') {
+				const daArray = daMove.split(',');
+				const daPlace = daArray[1] ? daArray[1].trim() : 'last';
+				const daBreakpoint = daArray[2] ? daArray[2].trim() : '767';
+				const daType = daArray[3] === 'min' ? daArray[3].trim() : 'max';
+				const daDestination = document.querySelector('.' + daArray[0].trim())
+				if (daArray.length > 0 && daDestination) {
+					daElement.setAttribute('data-da-index', number);
+					//Заполняем массив первоначальных позиций
+					originalPositions[number] = {
+						"parent": daElement.parentNode,
+						"index": indexInParent(daElement)
+					};
+					//Заполняем массив элементов 
+					daElementsArray[number] = {
+						"element": daElement,
+						"destination": document.querySelector('.' + daArray[0].trim()),
+						"place": daPlace,
+						"breakpoint": daBreakpoint,
+						"type": daType
+					}
+					number++;
 				}
-			},
-			//Scrolling
-			css3: true,
-			scrollingSpeed: 700,
-			autoScrolling: true,
-			fitToSection: true,
-			fitToSectionDelay: 1000,
-			scrollBar: false,
-			easing: 'easeInOutCubic',
-			easingcss3: 'ease',
-			loopBottom: false,
-			loopTop: false,
-			loopHorizontal: true,
-			continuousVertical: false,
-			continuousHorizontal: false,
-			scrollHorizontally: false,
-			interlockedSlides: false,
-			dragAndMove: false,
-			offsetSections: false,
-			resetSliders: false,
-			fadingEffect: false,
-			normalScrollElements: '#element1, .element2',
-			scrollOverflow: false,
-			scrollOverflowReset: false,
-			scrollOverflowOptions: null,
-			touchSensitivity: 15,
-			normalScrollElementTouchThreshold: 5,
-			bigSectionsDestination: null,
-
-			//Accessibility
-			keyboardScrolling: true,
-			animateAnchor: true,
-			recordHistory: true,
-
-			//Design
-			controlArrows: true,
-			paddingTop: '0',
-			paddingBottom: '0',
-			responsiveWidth: 0,
-			responsiveHeight: 0,
-			responsiveSlides: false,
-			parallax: false,
-			parallaxOptions: { type: 'reveal', percentage: 62, property: 'translate' },
-
-			//Custom selectors
-			sectionSelector: '.section',
-			slideSelector: '.slide',
-
-
-			//events
-			onLeave: function (index, nextIndex, direction) { },
-			afterLoad: function (anchorLink, index) { },
-			afterRender: function () { },
-			afterResize: function () { },
-			afterResponsive: function (isResponsive) { },
-			afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) { },
-			onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) { }
-		});
-	});
-} else {
-	const links = document.querySelectorAll("a.link");
-
-	for (const link of links) {
-		link.addEventListener("click", clickHandler);
-	}
-
-	function clickHandler(e) {
-		e.preventDefault();
-		const href = this.getAttribute("href");
-		const offsetTop = document.querySelector(href).offsetTop;
-
-		scroll({
-			top: offsetTop,
-			behavior: "smooth"
-		});
-	}
-
-
-	const burger = document.querySelector('.header__burger');
-	const menu = document.querySelector('.header__nav');
-	const menuItem = document.querySelectorAll('.nav-header__item');
-
-	const dark = document.querySelector('.bg-mob');
-
-	burger.onclick = function () {
-		burger.classList.toggle('active');
-		dark.classList.toggle('active');
-		menu.classList.toggle('active');
-	}
-
-	dark.onclick = function () {
-		burger.classList.toggle('active');
-		dark.classList.toggle('active');
-		menu.classList.toggle('active');
-	}
-
-	menuItem.forEach(function (el) {
-		el.onclick = function () {
-			burger.classList.toggle('active');
-			dark.classList.toggle('active');
-			menu.classList.toggle('active');
+			}
 		}
+		dynamicAdaptSort(daElementsArray);
+
+		//Создаем события в точке брейкпоинта
+		for (let index = 0; index < daElementsArray.length; index++) {
+			const el = daElementsArray[index];
+			const daBreakpoint = el.breakpoint;
+			const daType = el.type;
+
+			daMatchMedia.push(window.matchMedia("(" + daType + "-width: " + daBreakpoint + "px)"));
+			daMatchMedia[index].addListener(dynamicAdapt);
+		}
+	}
+	//Основная функция
+	function dynamicAdapt(e) {
+		for (let index = 0; index < daElementsArray.length; index++) {
+			const el = daElementsArray[index];
+			const daElement = el.element;
+			const daDestination = el.destination;
+			const daPlace = el.place;
+			const daBreakpoint = el.breakpoint;
+			const daClassname = "_dynamic_adapt_" + daBreakpoint;
+
+			if (daMatchMedia[index].matches) {
+				//Перебрасываем элементы
+				if (!daElement.classList.contains(daClassname)) {
+					let actualIndex = indexOfElements(daDestination)[daPlace];
+					if (daPlace === 'first') {
+						actualIndex = indexOfElements(daDestination)[0];
+					} else if (daPlace === 'last') {
+						actualIndex = indexOfElements(daDestination)[indexOfElements(daDestination).length];
+					}
+					daDestination.insertBefore(daElement, daDestination.children[actualIndex]);
+					daElement.classList.add(daClassname);
+				}
+			} else {
+				//Возвращаем на место
+				if (daElement.classList.contains(daClassname)) {
+					dynamicAdaptBack(daElement);
+					daElement.classList.remove(daClassname);
+				}
+			}
+		}
+		//customAdapt();
+	}
+
+	//Вызов основной функции
+	dynamicAdapt();
+
+	//Функция возврата на место
+	function dynamicAdaptBack(el) {
+		const daIndex = el.getAttribute('data-da-index');
+		const originalPlace = originalPositions[daIndex];
+		const parentPlace = originalPlace['parent'];
+		const indexPlace = originalPlace['index'];
+		const actualIndex = indexOfElements(parentPlace, true)[indexPlace];
+		parentPlace.insertBefore(el, parentPlace.children[actualIndex]);
+	}
+	//Функция получения индекса внутри родителя
+	function indexInParent(el) {
+		var children = Array.prototype.slice.call(el.parentNode.children);
+		return children.indexOf(el);
+	}
+	//Функция получения массива индексов элементов внутри родителя 
+	function indexOfElements(parent, back) {
+		const children = parent.children;
+		const childrenArray = [];
+		for (let i = 0; i < children.length; i++) {
+			const childrenElement = children[i];
+			if (back) {
+				childrenArray.push(i);
+			} else {
+				//Исключая перенесенный элемент
+				if (childrenElement.getAttribute('data-da') == null) {
+					childrenArray.push(i);
+				}
+			}
+		}
+		return childrenArray;
+	}
+	//Сортировка объекта
+	function dynamicAdaptSort(arr) {
+		arr.sort(function (a, b) {
+			if (a.breakpoint > b.breakpoint) { return -1 } else { return 1 }
+		});
+		arr.sort(function (a, b) {
+			if (a.place > b.place) { return 1 } else { return -1 }
+		});
+	}
+	//Дополнительные сценарии адаптации
+	function customAdapt() {
+		//const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	}
+}());
+$(document).ready(function () {
+	$('#fullpage').fullpage({
+		//Navigation
+
+		lockAnchors: false,
+		anchors: ['onePage', 'twoPage', 'threePage', 'fourPage', 'fivePage', 'sixPage'],
+		navigation: true,
+		navigationPosition: 'left',
+
+		//Scrolling
+		css3: true,
+		scrollingSpeed: 700,
+		autoScrolling: true,
+		fitToSection: true,
+		fitToSectionDelay: 1000,
+		scrollBar: false,
+		easing: 'easeInOutCubic',
+		easingcss3: 'ease',
+		loopBottom: false,
+		loopTop: false,
+		loopHorizontal: true,
+		continuousVertical: false,
+		continuousHorizontal: false,
+		scrollHorizontally: false,
+		interlockedSlides: false,
+		dragAndMove: false,
+		offsetSections: false,
+		resetSliders: false,
+		fadingEffect: false,
+		normalScrollElements: '#element1, .element2',
+		scrollOverflow: false,
+		scrollOverflowReset: false,
+		scrollOverflowOptions: null,
+		touchSensitivity: 15,
+		normalScrollElementTouchThreshold: 5,
+		bigSectionsDestination: null,
+
+		//Accessibility
+		keyboardScrolling: true,
+		animateAnchor: true,
+		recordHistory: true,
+
+		//Design
+		controlArrows: true,
+		paddingTop: '0',
+		paddingBottom: '0',
+		responsiveWidth: 770,
+		responsiveHeight: 0,
+		responsiveSlides: false,
+		parallax: false,
+		parallaxOptions: { type: 'reveal', percentage: 62, property: 'translate' },
+
+		//Custom selectors
+		sectionSelector: '.section',
+		slideSelector: '.slide',
+
+
+
+
+		//events
+		onLeave: function (index, nextIndex, direction) { },
+		afterLoad: function (anchorLink, index) { },
+		afterRender: function () { },
+		afterResize: function () { },
+		afterResponsive: function (isResponsive) { },
+		afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) { },
+		onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) { }
 	});
+});
+
+
+
+
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__nav');
+const menuItem = document.querySelectorAll('.nav-header__item');
+
+const dark = document.querySelector('.bg-mob');
+
+burger.onclick = function () {
+	burger.classList.toggle('active');
+	dark.classList.toggle('active');
+	menu.classList.toggle('active');
 }
+
+dark.onclick = function () {
+	burger.classList.toggle('active');
+	dark.classList.toggle('active');
+	menu.classList.toggle('active');
+}
+
+menuItem.forEach(function (el) {
+	el.onclick = function () {
+		burger.classList.toggle('active');
+		dark.classList.toggle('active');
+		menu.classList.toggle('active');
+	}
+});
+
 
 
 
